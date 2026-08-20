13fdbab0f44f99964fab3c72bbd42fee3054f132
Deepank Singh <deepank.singh.ece23@itbhu.ac.in>
2026-08-20 22:21:05 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,15 @@ int main() {
     if(command.substr(0,4) == "echo") {
       std::cout << command.substr(5) << std::endl;
     }
+    if(command.substr(0,4) == "type") {
+      std::string argument = command.substr(5);
+      if(argument == "echo" || argument == "type" || argument == "exit"){
+        std::cout << argument << " is a shell builtin" << std::endl;
+      }
+      else{
+        std::cout << argument << ": command not found" << std::endl;  
+      }
+    }
     else{
       std::cout << command << ": command not found" << std::endl;
     }
