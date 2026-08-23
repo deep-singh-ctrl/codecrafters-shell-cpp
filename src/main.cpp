cada6946b90a2d2920c261f5c1281403efed5693
Deepank Singh <deepank.singh.ece23@itbhu.ac.in>
2026-08-23 20:30:33 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
+#include <vector>
 
 
 
@@ -23,11 +25,51 @@ int main() {
         std::cout << argument << " is a shell builtin" << std::endl;
       }
       else{
+
         std::cout << argument << ": not found" << std::endl;  
       }
     }
     else{
+      //from this point on the shell should check for the command in all directories 
+      // of the path from left to right and print the match with executable
+      // permissions.
+          // Read environment variable (e.g., PATH or a custom one)
+    const char* env_p = std::getenv("PATH");
+    
+    if (env_p == nullptr) {
+      std::cout << "Environment variable not found." << std::endl;
+      return 1;
+    }
+
+    std::string env_val(env_p);
+    std::stringstream ss(env_val);
+    std::string token;
+    
+    // For LINUX the delimiter for PATH is a colon
+    char delimiter = ':'; 
+    std::vector<std::string> results;
+    
+    while (std::getline(ss, token, delimiter)) {
+      results.push_back(token);
+    }
+    bool valid = false;
+    for(std::string &s: results){
+      std::string filePath = s + command;
+      if(std::filesystem::exists(filePath)){
+        namespace fs = std::filesystem;
+        if ((fs::status(filePath).permissions() & fs::perms::owner_exec) != fs::perms::none
+            && (fs::status(filePath).permissions() & fs::perms::others_exec) != fs::perms::none
+            && (fs::status(filePath).permissions() & fs::perms::group_exec) != fs::perms::none)
+            {
+            std::cout << command << " is " << filePath << std::endl;
+            valid = true;
+            break;
+        } 
+      }
+    }
+    if(!valid){
       std::cout << command << ": command not found" << std::endl;
+    }
     }
   }
 }
